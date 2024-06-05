22151fecabd9f65a8ec5d6cee8f5413657512702
AndreaVomero99 <169089365+AndreaVomero99@users.noreply.github.com>
2024-06-05 15:33:11 +0200

    Update exit_status.sh

--- exit_status.sh
+++ exit_status.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
  
-echo hello # Primo comando inserito
-echo $?    # Exit status 0 returned because command executed successfully. # Stampando $?, otteniamo lo status per il comando eseguito in precedenza, in questo caso 0
+echo hello # First command entered
+echo $?    # Exit status 0 returned because command executed successfully. # By printing $?, we get the status of the previously executed command, in this case, 0
  
-lskdf      # Unrecognized command. # Questo comando non esiste
-echo $?    # Non-zero exit status returned -- command failed to execute. # E darà quindi il codice di status 127, per comando non trovato
+lskdf      # Unrecognized command. # This command does not exist
+echo $?    # Non-zero exit status returned -- command failed to execute. # And it will return the status code 127 for command not found
  
-echo # Linea vuota per leggibilità
+echo # Blank line for readability
  
-exit 113   # Will return 113 to shell #Così si chiede di stampare 113 per il comando exit
-           # To verify this, type "echo $?" after script terminates.
+exit 113   # Will return 113 to shell # This asks to return 113 for the exit command
+           # To verify this, type "echo $?" after the script terminates.
  
-#  By convention, an 'exit 0' indicates success,
-#+ while a non-zero exit value means an error or anomalous condition.
-#  See the "Exit Codes With Special Meanings" appendix.
+# By convention, an 'exit 0' indicates success,
+# while a non-zero exit value means an error or anomalous condition.
+# See the "Exit Codes With Special Meanings" appendix.
