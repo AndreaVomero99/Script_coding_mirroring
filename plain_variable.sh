d33345179c458252fba1558f478e93b21d41ac6a
AndreaVomero99 <169089365+AndreaVomero99@users.noreply.github.com>
2024-06-05 15:30:27 +0200

    Update plain_variable.sh

--- plain_variable.sh
+++ plain_variable.sh
@@ -7,30 +7,30 @@ echo
 # When it is being assigned, rather than referenced.
  
 # Assignment
-a=879 # Viene assegnato il valore 879 ad a
-echo "The value of \"a\" is $a." # E stampatone il contenuto
+a=879 # The value 879 is assigned to a
+echo "The value of \"a\" is $a." # And its content is printed
  
 # Assignment using 'let'
-let a=16+5 # Si può anche assegnare il valore tramite let, in questo caso immettendo come valore il risultato della somma
+let a=16+5 # The value can also be assigned using let, in this case, inputting the result of the addition
 echo "The value of \"a\" is now $a."
  
-echo # Linea vuota per leggibilità
+echo # Blank line for readability
  
 # In a 'for' loop (really, a type of disguised assignment):
-echo -n "Values of \"a\" in the loop are: " # Con -n si evita di andare a capo
-for a in 7 8 9 11 # Vengono assegnati a turno ad a i seguenti valori
-do # Il do si potrebbe anche omettere in un ciclo for, mentre è necessario in un ciclo while
-  echo -n "$a " # Stampa i valori assegnati, sempre evitando di andare a capo
-done # E si chiude il blocco iterativo
+echo -n "Values of \"a\" in the loop are: " # With -n, it prevents the line break
+for a in 7 8 9 11 # The following values are assigned to a in turn
+do # The do can be omitted in a for loop, while it is necessary in a while loop
+  echo -n "$a " # Prints the assigned values, always avoiding a line break
+done # Closes the iterative block
  
 echo
-echo # Doppia linea vuota per leggibilità
+echo # Double blank line for readability
  
 # In a 'read' statement (also a type of assignment):
-echo -n "Enter \"a\" " # Chiede input all'utente, e di inserire un valore quindi di a, il tutto senza andare a capo
-read a # Legge il valore dato dall'utente
-echo "The value of \"a\" is now $a." # E lo stampa
+echo -n "Enter \"a\" " # Asks for user input to enter a value for a, without a line break
+read a # Reads the value given by the user
+echo "The value of \"a\" is now $a." # And prints it
  
 echo
  
-exit 0 # e si chiude
+exit 0 # Ends the script
