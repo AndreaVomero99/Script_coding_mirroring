6335beb6f599d6ce86fc2a78258a4863e1a1d8d7
AndreaVomero99 <169089365+AndreaVomero99@users.noreply.github.com>
2024-06-05 15:25:17 +0200

    Update am_i_root.sh

--- am_i_root.sh
+++ am_i_root.sh
@@ -3,26 +3,26 @@
  
 ROOT_UID=0   # Root has $UID 0.
  
-if [ "$UID" -eq "$ROOT_UID" ]  # Con lo statement if compariamo la variabile UID a quella ROOT_UID, se la prima corrispondesse zero...
-then # ..allora...
-  echo "You are root." # Ci verrebbe confermato il nostro essere root
-else # altrimenti...
-  echo "You are just an ordinary user (but mom loves you just the same)." # Ci informano che non siamo effettivamente root
-fi # Chiusura del blocco condizionale 
+if [ "$UID" -eq "$ROOT_UID" ]  # With the initial if statement, we are checking if the UID variable equals the ROOT_UID variable, should UID be 0...
+then # 
+  echo "You are root." # We would get a confirmation that yes, we are logged in as the root user
+else # If the user id, the UID variable, is any number but zero...
+  echo "You are just an ordinary user (but mom loves you just the same)." # We get notified that we are in fact not the root user
+fi # The "fi" keyword closes the conditional block
  
-exit 0 # Ed uscita dallo script
+exit 0 # And the script gets then terminated with the "exit"
  
 # ============================================================= #
 # Code below will not execute, because the script already exited.
  
 # An alternate method of getting to the root of matters:
  
-ROOTUSER_NAME=root # Questa volta la variabile viene impostata non numericamente ma con testo
+ROOTUSER_NAME=root # This time we set the variable not as a number, but as a string
  
-username=`id -nu` # Con questo comando e queste opzioni, la variabile username conterrà il nome utente attuale, si poteva ottenere anche con il comando whoami
-if [ "$username" = "$ROOTUSER_NAME" ] #Compara il nome utente attuale a "root", se uguale...
-then # ...allora...
-  echo "Rooty, toot, toot. You are root." # Sei root
-else # altrimenti
-  echo "You are just a regular fella." # No
-fi # E chiude il blocco condizionale
+username=`id -nu` # With the n and u options we get as an output of the id request the logged username, same output would be granted by the whoami command
+if [ "$username" = "$ROOTUSER_NAME" ] # So if the output of id -nu equals to "root", stored in the ROOTUSER_NAME variable...
+then 
+  echo "Rooty, toot, toot. You are root." # ... root user confirmed...
+else # 
+  echo "You are just a regular fella." # ... or not, should the two variables store different values
+fi 
